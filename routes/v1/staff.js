dbe63a6fe1e3fd6e6882851c79486ed56dbd6559
Rafal Sprengel <sprengel.rafal@gmail.com>
2025-09-28 21:25:11 +0100

    changes

--- routes/v1/staff.js
+++ routes/v1/staff.js
@@ -4,9 +4,9 @@ const authorize = require('../../middleware/authorize');
 const staffController = require('../../controllers/staffController');
 const router = express.Router();
 
-router.get('/', authentMiddleware, authorize(['admin']), staffController.getStaff);
-router.get('/roles', authentMiddleware, authorize(['admin']), staffController.getStaffRoles);
-router.get('/:id', authentMiddleware, authorize(['admin']), staffController.getSingleStaff);
+router.get('/', authentMiddleware,  authorize(['guest', 'member', 'moderator', 'admin']), staffController.getStaff);
+router.get('/roles', authentMiddleware, authorize(['guest', 'member', 'moderator', 'admin']), staffController.getStaffRoles);
+router.get('/:id', authentMiddleware,  authorize(['guest', 'member', 'moderator', 'admin']), staffController.getSingleStaff);
 router.put('/:id', authentMiddleware, authorize(['admin']), staffController.updateStaff);
 router.delete('/:id', authentMiddleware, authorize(['admin']), staffController.deleteStaff);
 
